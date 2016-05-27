3372f5794c505340bad0cf9d47ba8abc9d0d9855
Davy Peter Braun <hello@davybraun.com>
2016-05-27 15:03:58 +0200

    Stub test bot for Earnings app.

---
+++ earnings/test.py
@@ -0,0 +1,16 @@
+"""Test bot for Earnings."""
+# -*- coding: utf-8 -*-
+
+from ._builtin import Bot
+
+
+class PlayerBot(Bot):
+    """Player bot for testing Earnings."""
+
+    def play_round(self):
+        """Player test round."""
+        pass
+
+    def validate_play(self):
+        """Validate test round."""
+        pass
