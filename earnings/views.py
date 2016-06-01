1503760e93a83b6134731c7baee5220db5a44e9f
Davy Peter Braun <hello@davybraun.com>
2016-06-01 16:05:40 +0200

    Add comments.

--- earnings/views.py
+++ earnings/views.py
@@ -6,7 +6,7 @@ from ._builtin import Page
 
 
 class Calculate(Page):
-
+    """Calculate then redirect to last page."""
     form_model = models.Player
 
     def before_next_page(self):
@@ -21,6 +21,7 @@ class Earnings(Page):
     # form_fields = ['donation']
 
     def vars_for_template(self):
+        """Variable in template."""
         return {
             'payoff': self.player.payoff
         }
