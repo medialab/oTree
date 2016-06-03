200232a324150a4a9f835fef1c3dd5af53e3f2a5
Davy Peter Braun <hello@davybraun.com>
2016-06-03 14:42:59 +0200

    Fix IAT read error after Python 3. Better output. Need to work on metadata parsing.

--- iat_exporter.py
+++ iat_exporter.py
@@ -12,25 +12,43 @@ results = []
 errors = []
 
 
+# Arguments for CLI are:
+# - file to parse
+# - label to add to filename (optional)
+# - path to directory to save to (optional, default to "./exports_iat")
+file_to_parse = sys.argv[1]
+file_label = ''
+export_dir = 'exports_iat'
+
+try:
+    file_label = sys.argv[2]
+except:
+    pass
+
+try:
+    export_dir = sys.argv[3]
+except:
+    pass
+
+
 def write_meta_file(meta):
     """Write down meta CSV file."""
-    for m in meta:
-        filename = (
-            'exports_iat/META__' + m[1][3] + '__' +
-            m[1][1] + '__' + m[1][2]
-        )
-        with open(filename + '.csv', 'w') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',', quotechar='|')
-            writer.writerows(m)
+    filename = (
+        export_dir + '/IAT_metadata_' + file_label
+    )
+    with open(filename + '.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|')
+        writer.writerow(meta[0][0])
+        for m in meta:
+            writer.writerow(m[1])
 
 
 def write_file(res, stem):
     """Write down results CSV file."""
     filename = (
-        'exports_iat/' + stem + '__' + res[0][1][4] + '__' +
-        res[0][1][2] + '__' + res[0][1][3] + '__'
+        export_dir + '/IAT_' + stem + '_' + file_label
     )
-    with open(filename + '.csv', 'wb') as csvfile:
+    with open(filename + '.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='|')
         writer.writerow(res[0][0])
         for r in res:
@@ -44,100 +62,102 @@ def remove_html(value):
     ).replace('</span><br />', ' ')
 
 
-with open(sys.argv[1], 'r') as f:
+with open(file_to_parse, 'r') as f:
     # Index of current user.
     count = 0
 
     for line in f.readlines():
+        print(line)
         # Skip header line.
         if 'Participant._id_in_session' not in line:
             # First batch of data (meta - before actual IAT test).
             m = line[:line.find('"')]
 
-            # Get ID in session, code, label, time started.
+            # Get MTurk ID, code, label, time started.
             m = m.split(',')
             # Process only finished sessions.
             if 'Not visited yet' not in m[7]:
-                id_in_session = m[0].replace('\n', '')
+                mturk_id = m[15]
                 code = m[1].replace('\n', '')
                 label = m[2].replace('\n', '')
                 time_started = m[10].replace('\n', '')
 
-                meta.append([
-                    [
-                        'ID in session', 'Code', 'Label', 'Time started',
-                        'Trials order', 'Error percentage'
-                    ],
-                    [
-                        id_in_session, code, label, time_started
-                    ]
+                if mturk_id is not None and mturk_id != '':
+                    meta.append([
+                        [
+                            'MTurk ID', 'Code', 'Label', 'Time started',
+                            'Trials order', 'Error percentage'
+                        ],
+                        [
+                            mturk_id, code, label, time_started
+                        ]
 
-                ])
+                    ])
 
-                # Second batch of data (actual IAT results): isolate it.
-                data = line[line.find('"') + 1:line.rfind('"')]
-                data = data.replace('u\'', '\'')
+                    # Second batch of data (actual IAT results): isolate it.
+                    data = line[line.find('"') + 1:line.rfind('"')]
+                    data = data.replace('u\'', '\'')
 
-                try:
-                    # Deserialize data as dict.
-                    data = ast.literal_eval(data)
+                    try:
+                        # Deserialize data as dict.
+                        data = ast.literal_eval(data)
 
-                    # Get more metadata: trials order, % of errors.
-                    meta[count][1].append(data['order'])
-                    meta[count][1].append(data['error_percentage'])
+                        # Get more metadata: trials order, % of errors.
+                        meta[count][1].append(data['order'])
+                        meta[count][1].append(data['error_percentage'])
 
-                    # Process regular trials' results:
-                    trials_results = data['results']
-                    for r in trials_results:
-                        results.append([
-                            [
-                                'Trial ID', 'Player ID in session', 'Code',
-                                'Label', 'Time started', 'Left category',
-                                'Right category', 'Stimuli word',
-                                'Correct position', 'Correct category',
-                                'Time taken'
-                            ],
-                            [
-                                remove_html(r['id']),
-                                id_in_session, code, label, time_started,
-                                remove_html(r['left']),
-                                remove_html(r['right']),
-                                remove_html(r['stimuli']),
-                                remove_html(r['correctPosition']),
-                                remove_html(r['correctCategory']),
-                                r['timing']
-                            ]
-                        ])
+                        # Process regular trials' results:
+                        trials_results = data['results']
+                        for r in trials_results:
+                            results.append([
+                                [
+                                    'Trial ID', 'Player MTurk ID', 'Code',
+                                    'Label', 'Time started', 'Left category',
+                                    'Right category', 'Stimuli word',
+                                    'Correct position', 'Correct category',
+                                    'Time taken'
+                                ],
+                                [
+                                    remove_html(r['id']),
+                                    mturk_id, code, label, time_started,
+                                    remove_html(r['left']),
+                                    remove_html(r['right']),
+                                    remove_html(r['stimuli']),
+                                    remove_html(r['correctPosition']),
+                                    remove_html(r['correctCategory']),
+                                    r['timing']
+                                ]
+                            ])
 
-                    # Process regular trials' errors:
-                    trials_results = data['errors']
-                    for r in trials_results:
-                        errors.append([
-                            [
-                                'Trial ID', 'ID in session', 'Code', 'Label',
-                                'Time started', 'Left category',
-                                'Right category', 'Stimuli word',
-                                'Correct position', 'Correct category',
-                                'Failed by time out', 'Time taken'
-                            ],
-                            [
-                                remove_html(r['id']),
-                                id_in_session, code, label, time_started,
-                                remove_html(r['left']),
-                                remove_html(r['right']),
-                                remove_html(r['stimuli']),
-                                remove_html(r['correctPosition']),
-                                remove_html(r['correctCategory']),
-                                r['timedOut'],
-                                r['timing']
-                            ]
-                        ])
-                except Exception as e:
-                    print(e)
+                        # Process regular trials' errors:
+                        trials_results = data['errors']
+                        for r in trials_results:
+                            errors.append([
+                                [
+                                    'Trial ID', 'MTurk ID', 'Code', 'Label',
+                                    'Time started', 'Left category',
+                                    'Right category', 'Stimuli word',
+                                    'Correct position', 'Correct category',
+                                    'Failed by time out', 'Time taken'
+                                ],
+                                [
+                                    remove_html(r['id']),
+                                    mturk_id, code, label, time_started,
+                                    remove_html(r['left']),
+                                    remove_html(r['right']),
+                                    remove_html(r['stimuli']),
+                                    remove_html(r['correctPosition']),
+                                    remove_html(r['correctCategory']),
+                                    r['timedOut'],
+                                    r['timing']
+                                ]
+                            ])
+                    except Exception as e:
+                        print(e)
 
                 count += 1
     f.close()
 
 write_meta_file(meta)
-write_file(results, 'RESULTS')
-write_file(errors, 'ERRORS')
+write_file(results, 'results')
+write_file(errors, 'errors')
