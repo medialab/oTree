338db9746b74c10c1cf1d3fa1e2c1152b15c6bad
Davy Peter Braun <hello@davybraun.com>
2016-09-14 11:43:14 +0200

    Fix unknown var in settings.

--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '_static_root'),
+    os.path.join(BASE_DIR, '_static_root'),
 )
 
 # the environment variable OTREE_PRODUCTION controls whether Django runs in
