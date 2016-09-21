636834a1063bcdbab1ddc579b1dbf582e866bf78
Davy Peter Braun <hello@davybraun.com>
2016-09-21 17:23:27 +0200

    Fix static root.

--- settings.py
+++ settings.py
@@ -12,13 +12,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '_static_root')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '_static_root'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '_static_root'),
+# )
 
 # the environment variable OTREE_PRODUCTION controls whether Django runs in
 # DEBUG mode. If OTREE_PRODUCTION==1, then DEBUG=False
