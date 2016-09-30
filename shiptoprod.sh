210aefee0d23757a291c42ffc33a953ca1082b30
Davy Peter Braun <hello@davybraun.com>
2016-09-30 14:00:26 +0200

    Update ship script.

--- shiptoprod.sh
+++ shiptoprod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git checkout deploy-fr-prod && git merge master && git push origin deploy-fr-prod
+git checkout deploy-fr-prod && git merge master && git push origin deploy-fr-prod && git checkout master
