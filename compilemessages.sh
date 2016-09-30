78dbcb42551bff1b8277caa6ba550d79288f9eb6
Davy Peter Braun <hello@davybraun.com>
2016-09-30 16:08:59 +0200

    Update/create scripts for i18n file batch management.

--- compilemessages.sh
+++ compilemessages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd trust && django-admin compilemessages && cd ../public_goods && django-admin compilemessages && cd ../dictator && django-admin compilemessages && cd ../
+cd trust && django-admin compilemessages && cd ../public_goods && django-admin compilemessages && cd ../dictator && django-admin compilemessages && cd ../iat && django-admin compilemessages && cd ../survey_i18n && django-admin compilemessages && cd ../
