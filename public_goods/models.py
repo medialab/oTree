a58d4ba6888439e4a16edde3128159d73161daf7
Davy Peter Braun <hello@davybraun.com>
2016-10-26 15:35:39 +0200

    Set dynamic ceiling on inputs for PG sim.

--- public_goods/models.py
+++ public_goods/models.py
@@ -31,7 +31,7 @@ links = {
 
 keywords = ("Public Goods",)
 
-allocated_amount = get_language()[:2] == 'fr' and 10 or 12000
+allocated_amount = get_language()[:2] == 'ko' and 12000 or 10
 
 
 class Constants(BaseConstants):
