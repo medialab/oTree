dfd5a6725b3c338290e1d007efcfb1772729df43
Davy Peter Braun <hello@davybraun.com>
2016-11-18 11:24:01 +0100

    Remove previous deployment script with new one for Korea.

---
+++ deploy-ko.sh
@@ -0,0 +1,1 @@
+git checkout deploy-ko-prod && git merge master && git push deploy-ko-prod && git checkout master
--- shiptoprod.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git checkout deploy-fr-prod && git merge master && git push origin deploy-fr-prod && git checkout master
