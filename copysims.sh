c74b097e82b979dd293572fc92b6dbb837846599
Davy Peter Braun <hello@davybraun.com>
2017-01-10 13:01:37 +0100

    Tool to copy exports from sims.

---
+++ copysims.sh
@@ -0,0 +1,3 @@
+cp ./_animatedsims/trust/output/trust_simulation.js ./_static/trust/ &&\
+cp ./_animatedsims/dictator/output/dictator_simulation.js ./_static/dictator/ &&\
+cp ./_animatedsims/public_goods/output/public_goods_simulation.js ./_static/public_goods/
