dbe24c9a198bbe68f69272cfb9d6d2e94f3564f1
Davy Peter Braun <hello@davybraun.com>
2017-02-21 17:00:16 +0100

    Fix trust sim limits.

--- _animatedsims/trust/output/trust_simulation.js
+++ _animatedsims/trust/output/trust_simulation.js
@@ -1,70 +1,9 @@
 (function (lib, img, cjs, ss, an) {
 
 var p; // shortcut to reference prototypes
-lib.webFontTxtInst = {}; 
-var loadedTypekitCount = 0;
-var loadedGoogleCount = 0;
-var gFontsUpdateCacheList = [];
-var tFontsUpdateCacheList = [];
 lib.ssMetadata = [];
 
 
-
-lib.updateListCache = function (cacheList) {		
-	for(var i = 0; i < cacheList.length; i++) {		
-		if(cacheList[i].cacheCanvas)		
-			cacheList[i].updateCache();		
-	}		
-};		
-
-lib.addElementsToCache = function (textInst, cacheList) {		
-	var cur = textInst;		
-	while(cur != exportRoot) {		
-		if(cacheList.indexOf(cur) != -1)		
-			break;		
-		cur = cur.parent;		
-	}		
-	if(cur != exportRoot) {		
-		var cur2 = textInst;		
-		var index = cacheList.indexOf(cur);		
-		while(cur2 != cur) {		
-			cacheList.splice(index, 0, cur2);		
-			cur2 = cur2.parent;		
-			index++;		
-		}		
-	}		
-	else {		
-		cur = textInst;		
-		while(cur != exportRoot) {		
-			cacheList.push(cur);		
-			cur = cur.parent;		
-		}		
-	}		
-};		
-
-lib.gfontAvailable = function(family, totalGoogleCount) {		
-	lib.properties.webfonts[family] = true;		
-	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
-	for(var f = 0; f < txtInst.length; ++f)		
-		lib.addElementsToCache(txtInst[f], gFontsUpdateCacheList);		
-
-	loadedGoogleCount++;		
-	if(loadedGoogleCount == totalGoogleCount) {		
-		lib.updateListCache(gFontsUpdateCacheList);		
-	}		
-};		
-
-lib.tfontAvailable = function(family, totalTypekitCount) {		
-	lib.properties.webfonts[family] = true;		
-	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
-	for(var f = 0; f < txtInst.length; ++f)		
-		lib.addElementsToCache(txtInst[f], tFontsUpdateCacheList);		
-
-	loadedTypekitCount++;		
-	if(loadedTypekitCount == totalTypekitCount) {		
-		lib.updateListCache(tFontsUpdateCacheList);		
-	}		
-};
 // symbols:
 // helper functions:
 
@@ -1471,6 +1410,7 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{ini
 		
 		  function getMaxPlayerMoney(index) {
 		    return function () {
+			  console.log(index, players.getPlayers()[index].money)
 		      return players.getPlayers()[index].money;
 		    };
 		  }
@@ -1493,7 +1433,7 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{ini
 		    getMaxCart1Money: getMaxCartMoney.call(this, 0),
 		    getMaxCart2Money: getMaxCartMoney.call(this, 1),
 		    getMaxPlayer1Money: getMaxPlayerMoney.call(this, 0),
-		    getMaxPlayer2Money: getMaxPlayerMoney.call(this, 0)
+		    getMaxPlayer2Money: getMaxPlayerMoney.call(this, 1)
 		  };
 		}
 		
@@ -1762,6 +1702,7 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{ini
 	this.frame_191 = function() {
 		this.stop();
 		window.TRUST.operate.dispatchGainToPlayer2();
+		console.log(window.TRUST.operate.getMaxPlayer2Money());
 	}
 	this.frame_234 = function() {
 		this.stop();
@@ -2090,7 +2031,6 @@ lib.properties = {
 	fps: 30,
 	color: "#CCD1DB",
 	opacity: 1.00,
-	webfonts: {},
 	manifest: [],
 	preloads: []
 };
