f9a90d7d9d79a166e7e74ae490e0e8351f48bf5c
Davy Peter Braun <hello@davybraun.com>
2017-02-23 17:28:10 +0100

    Prepare demo.

--- trustlab_apps_config.py
+++ trustlab_apps_config.py
@@ -88,6 +88,18 @@ SESSION_CONFIGS = [
         'quota_redirects': quota_redirects_ko,
     },
     {
+        'name': 'test_iat',
+        'display_name': 'IAT',
+        'num_demo_participants': 100,
+        'app_sequence': [
+            'iat'
+        ],
+        'treatment': 'A1',
+        'payoff_group': 0,
+        'speedsters_threshold': 1,
+        'quota_redirects': quota_redirects_ko,
+    },
+    {
         'name': 'test_survey',
         'display_name': 'SURVEY',
         'num_demo_participants': 100,
@@ -101,8 +113,8 @@ SESSION_CONFIGS = [
     },
 
     {
-        'name': 'trustlab_2016_A1_ko_prod',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path A1 | Payoff Group 2 (production)',
+        'name': 'trustlab_2016_A1_sl_prod',
+        'display_name': 'TRUSTLAB | February 2017 | Slovenia | path 1 | Payoff Group 2 (production)',
         'num_demo_participants': 126,
         'app_sequence': [
             'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
@@ -115,8 +127,8 @@ SESSION_CONFIGS = [
         'quota_redirects': quota_redirects_ko,
     },
     {
-        'name': 'trustlab_2016_A2_ko_prod',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path A2 | Payoff Group 2 (production)',
+        'name': 'trustlab_2016_A2_sl_prod',
+        'display_name': 'TRUSTLAB | February 2017 | Slovenia | path 2 | Payoff Group 2 (production)',
         'num_demo_participants': 126,
         'app_sequence': [
             'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
@@ -125,41 +137,13 @@ SESSION_CONFIGS = [
         ],
         'speedsters_threshold': 10 * 60,
         'treatment': 'A2',
-        'payoff_group': 2,
-        'quota_redirects': quota_redirects_ko,
-    },
-    {
-        'name': 'trustlab_2016_B1_ko_prod',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path B1 | Payoff Group 2 (production)',
-        'num_demo_participants': 126,
-        'app_sequence': [
-            'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
-            'public_goods', 'dictator', 'iat', 'survey_i18n', 'timer_stop',
-            'redirect_speedsters', 'earnings', 'redirect_completes'
-        ],
-        'speedsters_threshold': 10 * 60,
-        'treatment': 'B1',
-        'payoff_group': 2,
-        'quota_redirects': quota_redirects_ko,
-    },
-    {
-        'name': 'trustlab_2016_B2_ko_prod',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path B2 | Payoff Group 2 (production)',
-        'num_demo_participants': 126,
-        'app_sequence': [
-            'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
-            'public_goods', 'dictator', 'iat', 'survey_i18n', 'timer_stop',
-            'redirect_speedsters', 'earnings', 'redirect_completes'
-        ],
-        'speedsters_threshold': 10 * 60,
-        'treatment': 'B2',
         'payoff_group': 2,
         'quota_redirects': quota_redirects_ko,
     },
 
     {
-        'name': 'trustlab_2016_A1_ko',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path A1',
+        'name': 'trustlab_2016_A1_sl',
+        'display_name': 'TRUSTLAB | February 2017 | Slovenia | path 1',
         'num_demo_participants': 126,
         'app_sequence': [
             'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
@@ -172,8 +156,8 @@ SESSION_CONFIGS = [
         'quota_redirects': quota_redirects_ko,
     },
     {
-        'name': 'trustlab_2016_A2_ko',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path A2',
+        'name': 'trustlab_2016_A2_sl',
+        'display_name': 'TRUSTLAB | February 2017 | Slovenia | path 2',
         'num_demo_participants': 126,
         'app_sequence': [
             'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
@@ -182,34 +166,6 @@ SESSION_CONFIGS = [
         ],
         'speedsters_threshold': 1,
         'treatment': 'A2',
-        'payoff_group': 1,
-        'quota_redirects': quota_redirects_ko,
-    },
-    {
-        'name': 'trustlab_2016_B1_ko',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path B1',
-        'num_demo_participants': 126,
-        'app_sequence': [
-            'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
-            'public_goods', 'dictator', 'iat', 'survey_i18n', 'timer_stop',
-            'redirect_speedsters', 'earnings', 'redirect_completes'
-        ],
-        'speedsters_threshold': 1,
-        'treatment': 'B1',
-        'payoff_group': 1,
-        'quota_redirects': quota_redirects_ko,
-    },
-    {
-        'name': 'trustlab_2016_B2_ko',
-        'display_name': 'TRUSTLAB | Sept 2016 | KO | path B2',
-        'num_demo_participants': 126,
-        'app_sequence': [
-            'mandatory_participant_label', 'timer_start', 'introduction', 'trust',
-            'public_goods', 'dictator', 'iat', 'survey_i18n', 'timer_stop',
-            'redirect_speedsters', 'earnings', 'redirect_completes'
-        ],
-        'speedsters_threshold': 1,
-        'treatment': 'B2',
         'payoff_group': 1,
         'quota_redirects': quota_redirects_ko,
     }
